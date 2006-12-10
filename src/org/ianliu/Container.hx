d57bc72bfea403362ed6dcf212656f89ba032699
ian.liu88 <ian.liu88@a3a42178-4524-0410-9ec1-017474eea0cb>
2006-12-10 21:03:55 +0000

    Added a folder remotely

---
+++ src/org/ianliu/Container.hx
@@ -0,0 +1,61 @@
+
+
+package org.ianliu;
+
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.events.EventDispatcher;
+import org.ianliu.Grid;
+
+class Container extends org.ianliu.UIComponent {
+	
+	public var layout:Grid;
+	public var padding:Int;
+	
+	private var innerWidth :Float;
+	private var innerHeight:Float;
+	private var bdr:Sprite;
+	
+	public function new(?layout:Grid, ?has_border:Bool, ?padding:Int) {
+		super();
+		addEventListener(UIComponent.REFRESH, refresh);
+		this.layout  = if(layout == null) new Grid(1, FixedCols) else layout;
+		this.padding = if(padding == null) 5 else padding;
+		this.layout.setMargin(this.padding);
+		if(has_border) {
+			bdr = new Sprite();
+			super.addChild(bdr);
+		}
+	}
+	
+	private function refresh(e:Event):Void {
+		layout.draw();
+		if(bdr != null) {
+			bdr.graphics.clear();
+			var w = innerWidth  = this.width;
+			var h = innerHeight = this.height;
+			bdr.graphics.lineStyle(1, 0xffffff, 1, true);
+			bdr.graphics.drawRoundRect(1, 1, w+2*padding, h+2*padding, 10, 10);
+			bdr.graphics.lineStyle(1, 0x787878, 1, true);
+			bdr.graphics.drawRoundRect(0, 0, w+2*padding, h+2*padding, 10, 10);
+		}
+	}
+	public function add(c:UIComponent):Void {
+		addChild(c);
+		layout.add(c);
+		dispatchEvent(new Event(UIComponent.REFRESH, true));
+	}
+	public function remove(c:UIComponent):Void {
+		super.removeChild(c);
+		layout.remove(c);
+		dispatchEvent(new Event(UIComponent.REFRESH, true));
+	}
+	public function removeAt(pos:Int):Void {
+		super.removeChildAt(pos);
+		//layout.removeAt(pos);
+		dispatchEvent(new Event(UIComponent.REFRESH, true));
+	}
+	
+	public function getInnerWidth() :Float { return if(bdr == null) this.width  else innerWidth;  }
+	public function getInnerHeight():Float { return if(bdr == null) this.height else innerHeight; }
+}
---
+++ src/org/ianliu/UIComponent.hx
@@ -0,0 +1,15 @@
+
+
+package org.ianliu;
+
+
+class UIComponent extends flash.display.Sprite
+{
+	public static var REFRESH:String = "uiRefresh";
+	
+	public var margin:Int;
+	
+	public function new() {
+		super();
+	}
+}
